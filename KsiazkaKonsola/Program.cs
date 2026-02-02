1761f7a464590627d1a57c25bdaadf8e910f8d7d
Student <Student@TE-KAL-D-S14-03>
2026-02-02 12:50:43 +0100

    Implementacja pobrania "książki" z pliku i splita na strony, testy(wyświetlanie wyniku w konsoli)

--- KsiazkaKonsola/Program.cs
+++ KsiazkaKonsola/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KsiazkaKonsola;
+
+string daneZPliku = File.ReadAllText(@"C:\Users\Student\Desktop\dane.txt");
+string[] tablicaStron = daneZPliku.Split(';');
+
+Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
+
+for (int i = 1; i < tablicaStron.Length; i++)
+{
+    ksiazka.DodajStrone(tablicaStron[i]);
+}
+
+foreach (Strona str in ksiazka.strony)
+{
+     if (str.numerStrony == 1)
+    {
+        Console.WriteLine($"Numer Strony: {str.numerStrony}\nTytuł: {str.trescStrony}");
+    }
+     else
+    {
+        Console.WriteLine($"Numer Strony: {str.numerStrony}\nTreść: {str.trescStrony}");
+    }
+    //Console.WriteLine($"Numer Strony: {str.numerStrony}\nTreść: {str.trescStrony}");
+}
