f193f4943943de278b3249c71301c9e69c5c49b4
Student <Student@TE-KAL-D-S14-03>
2026-02-02 12:02:56 +0100

    Implementacja klasy Ksiazka

---
+++ KsiazkaKonsola/Ksiazka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsiazkaKonsola
+{
+    internal class Ksiazka
+    {
+        public List<Strona> strony = new List<Strona>();
+
+        public Ksiazka(string tytul)
+        {
+            var strona = new Strona(tytul, 1);
+            strony.Add(strona);
+        }
+
+        public void DodajStrone(string trescStrony)
+        {
+            var strona = new Strona(trescStrony);
+            strony.Add(strona);
+        }
+    }
+}
