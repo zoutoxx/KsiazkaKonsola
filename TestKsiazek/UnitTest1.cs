37cb51d2626ee19af34e8fd3d5ff809f735db87c
Student <Student@TE-KAL-D-S14-03>
2026-02-02 13:27:53 +0100

    Implementacja dalszych testów jednostkowych

--- TestKsiazek/UnitTest1.cs
+++ TestKsiazek/UnitTest1.cs
@@ -15,7 +15,45 @@ namespace TestKsiazek
 
             //Assert
             Assert.AreEqual(ksiazka.strony[0].trescStrony, strona.trescStrony);
+        }
+        [TestMethod]
+        public void PorownanieOstatniejStrony()
+        {
+            //Arrange
+            string daneZPliku = File.ReadAllText(@"C:\Users\Student\Desktop\dane.txt");
+            string[] tablicaStron = daneZPliku.Split(';');
+
+            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
+
+            for (int i = 1; i < tablicaStron.Length; i++)
+            {
+                ksiazka.DodajStrone(tablicaStron[i]);
+            }
             
+            string daneDoTestowania = "Powrót do miasta nie by³ ju¿ ucieczk¹, lecz wyborem. Ulice wydawa³y siê jaœniejsze, a ludzie mniej obcy. Adam zatrzyma³ siê przy tym samym zegarze, który kiedyœ go niepokoi³. Tym razem uœmiechn¹³ siê, widz¹c jego niedok³adnoœæ. Czas nie by³ wrogiem, lecz towarzyszem. Wiedzia³, ¿e przed nim jeszcze wiele niewiadomych, ale nie ba³ siê ich jak dawniej. Z listem schowanym g³êboko w kieszeni ruszy³ przed siebie, gotów pisaæ dalsz¹ czêœæ w³asnej historii.";
+            //Act
+
+            //Assert
+            Assert.AreEqual(ksiazka.strony[ksiazka.strony.Count - 1].trescStrony.Trim(), daneDoTestowania.Trim());
+        }
+        [TestMethod]
+        public void PorownywanieCalejStrony()
+        {
+            //Arrange
+            string daneZPliku = File.ReadAllText(@"C:\Users\Student\Desktop\dane.txt");
+            string[] tablicaStron = daneZPliku.Split(';');
+
+            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
+
+            for (int i = 1; i < tablicaStron.Length; i++)
+            {
+                ksiazka.DodajStrone(tablicaStron[i]);
+            }
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(ksiazka.strony.Count, tablicaStron.Length);
         }
     }
 }
