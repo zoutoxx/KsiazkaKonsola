ee209e4e6d4509c90b7f621cc825e44cc238ab19
Student <Student@TE-KAL-D-S14-03>
2026-02-02 11:55:46 +0100

    Implementacja klasy Strona

---
+++ KsiazkaKonsola/Strona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsiazkaKonsola
+{
+    internal class Strona
+    {
+        public string trescStrony;
+        public uint numerStrony;
+        public static uint licznikStron = 0;
+
+        public Strona(string trescStrony, uint numerStrony)
+        {
+            this.trescStrony = trescStrony;
+            this.numerStrony = numerStrony;
+            licznikStron = numerStrony + 1;
+        }
+
+        public Strona(string trescStrony)
+        {
+            this.trescStrony = trescStrony;
+            this.numerStrony = licznikStron++;
+        }
+    }
+}
